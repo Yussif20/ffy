423b8cc7c2d3b5402ac2865fe3429856b71fed82
Yousef Ayman <yousefayman2000500@gmail.com>
2026-02-17 22:48:01 +0200

    enhance the how it works styles

--- src/components/Global/SectionTitle.tsx
+++ src/components/Global/SectionTitle.tsx
@@ -5,14 +5,16 @@ export default function SectionTitle({
   subtitle,
   subtitleClass,
   titleClass,
+  className,
 }: {
   title: string;
   subtitle?: string;
   subtitleClass?: string;
   titleClass?: string;
+  className?: string;
 }) {
   return (
-    <div className="text-center flex justify-center items-center flex-col gap-3">
+    <div className={cn("text-center flex justify-center items-center flex-col gap-3", className)}>
       <h1 className={cn("font-bold text-3xl lg:text-4xl xl:text-5xl max-w-2xl", titleClass)}>
         {title}
       </h1>
