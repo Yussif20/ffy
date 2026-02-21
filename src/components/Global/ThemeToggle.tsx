6ba0aa4a261d32117f113219fccb4f374adecbfb
yussif20 <yousefayman2000500@gmail.com>
2026-02-21 23:12:52 +0200

    remove the unused imports in the theme toggle to fix the build bug

--- src/components/Global/ThemeToggle.tsx
+++ src/components/Global/ThemeToggle.tsx
@@ -8,7 +8,6 @@ import { useEffect, useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
