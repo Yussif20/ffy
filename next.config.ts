5af6f943e329b7fcce17021757ba188a38a90b9b
Ahmed Younis <Ahmed.M.Younis20@gmail.com>
2026-03-05 07:44:37 +0000

    update build to be standalone

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     optimizePackageImports: [
       "lucide-react",
